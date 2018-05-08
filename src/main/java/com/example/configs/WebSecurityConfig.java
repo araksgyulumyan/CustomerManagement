7688bc45c2eb6cd0f37988eb18424d1a2299d645
araksgyulumyan <araks.gyulumyan@gmail.com>
2018-05-08 22:25:54 +0400

    Add basic config to run project

--- src/main/java/com/example/configs/WebSecurityConfig.java
+++ src/main/java/com/example/configs/WebSecurityConfig.java
@@ -1,55 +1,55 @@
-package com.example.configs;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-/**
- * Created by araksgyulumyan
- * Date - 5/8/18
- * Time - 5:42 PM
- */
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-
-    // todo in future - write customUserDetailsService
-//    @Autowired
-//    @Qualifier("customUserDetailsService")
-//    UserDetailsService userDetailsService;
+//package com.example.configs;
 //
-//    @Autowired
-//    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService);
-//        auth.authenticationProvider(authenticationProvider());
-//    }
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 //
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+///**
+// * Created by araksgyulumyan
+// * Date - 5/8/18
+// * Time - 5:42 PM
+// */
 //
-//    @Bean
-//    public DaoAuthenticationProvider authenticationProvider() {
-//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-//        authenticationProvider.setUserDetailsService(userDetailsService);
-//        authenticationProvider.setPasswordEncoder(passwordEncoder());
-//        return authenticationProvider;
-//    }
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/", "/home").permitAll()
-//                .antMatchers("/admin/**","/newuser").access("hasRole('ADMIN')")
-//                .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .usernameParameter("username").passwordParameter("password")
-//                .and().csrf()
-//                .and().exceptionHandling().accessDeniedPage("/Access_Denied");
-//    }
-}
+//
+//    // todo in future - write customUserDetailsService
+////    @Autowired
+////    @Qualifier("customUserDetailsService")
+////    UserDetailsService userDetailsService;
+////
+////    @Autowired
+////    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+////        auth.userDetailsService(userDetailsService);
+////        auth.authenticationProvider(authenticationProvider());
+////    }
+////
+////    @Bean
+////    public PasswordEncoder passwordEncoder() {
+////        return new BCryptPasswordEncoder();
+////    }
+////
+////    @Bean
+////    public DaoAuthenticationProvider authenticationProvider() {
+////        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+////        authenticationProvider.setUserDetailsService(userDetailsService);
+////        authenticationProvider.setPasswordEncoder(passwordEncoder());
+////        return authenticationProvider;
+////    }
+////
+////    @Override
+////    protected void configure(HttpSecurity http) throws Exception {
+////        http.authorizeRequests()
+////                .antMatchers("/", "/home").permitAll()
+////                .antMatchers("/admin/**","/newuser").access("hasRole('ADMIN')")
+////                .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+////                .and()
+////                .formLogin()
+////                .loginPage("/login")
+////                .usernameParameter("username").passwordParameter("password")
+////                .and().csrf()
+////                .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+////    }
+//}
